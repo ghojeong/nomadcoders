5ef424b44b271f2f9209dcf504005f5c010cf57f
고정완 (Can) <jw.gho@ecubelabs.com>
2021-05-15 08:48:35 +0900

    feat: Basic DOM practcie

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 console.log("Hello world!");
+
+// const title = document.getElementById("title");
+const title = document.querySelector("#title");
+title.innerHTML = "Put Html like this";
+title.style.color = "red";
+
+console.log(title);
+console.dir(title);
+
+document.title = "Hello World";
+
+window.addEventListener("resize", function (event) {
+  console.log(event);
+});
+
+title.addEventListener("click", function () {
+  console.log(title.style.color);
+  title.style.color = title.style.color === "blue" ? "red" : "blue";
+});
+
+const age = prompt("How old are you");
+console.log(age);
