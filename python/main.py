888ce4c76c30bc1cc9428d808b3981193a5d5e0c
Pyro(고정완) <rhwjddhks@gmail.com>
2021-07-16 11:12:15 +0900

    feat: 스크래핑 한 결과를 csv 로 저장

--- python/main.py
+++ python/main.py
@@ -1,5 +1,6 @@
 from indeed import get_jobs as get_indeed_jobs
 from stackoverflow import get_jobs as get_so_jobs
+from save import save_to_file
 
 # indeed_jobs = get_indeed_jobs()
 # print(indeed_jobs)
@@ -7,6 +8,8 @@ from stackoverflow import get_jobs as get_so_jobs
 # print(len(indeed_jobs))
 
 so_jobs = get_so_jobs()
-print(so_jobs)
+# print(so_jobs)
 # print()
 # print(len(so_jobs))
+
+save_to_file(so_jobs)
