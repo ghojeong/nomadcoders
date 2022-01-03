80528d3917ac04634aad3ac09dbc6283c7be486f
Pyro <rhwjddhks@gmail.com>
2022-01-03 18:37:11 +0900

    2.0 Patterns

--- nextjs-intro/pages/_app.tsx
+++ nextjs-intro/pages/_app.tsx
@@ -1,22 +1,11 @@
 import type { AppProps } from "next/app";
-import NavBar from "../components/NavBar";
+import Layout from "../components/Layout";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
-      <footer>This is Footer</footer>
-      <style jsx global>{`
-        nav {
-          background-color: black;
-        }
-        a {
-          color: white;
-          text-decoration: none;
-        }
-      `}</style>
-    </>
+    </Layout>
   );
 }
--- nextjs-intro/pages/index.tsx
+++ nextjs-intro/pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from "next";
-import NavBar from "../components/NavBar";
+import Head from "next/head";
+import Seo from "../components/Seo";
 
 const Home: NextPage = () => {
   return (
     <div>
+      <Seo title="Home" />
       <h1 className="active">Home</h1>
     </div>
   );
