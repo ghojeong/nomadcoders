907675cc7700fdba34ba710dc2004a643730cc4e
Pyro(JeongWan Gho) <rhwjddhks@gmail.com>
2025-03-07 13:16:05 +0900

    feat: Add movies.controller and service

---
+++ hi-nest/src/movies/entities/movie.entity.ts
@@ -0,0 +1,6 @@
+export class Movie {
+  id: number;
+  titie: string;
+  year: number;
+  genres: string[];
+}
---
+++ hi-nest/src/movies/movies.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+
+  @Get('search')
+  search(@Query('year') searchingYear: number) {
+    return `We are searching for a movie made after: ${searchingYear}`;
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    return this.moviesService.create(movieData);
+  }
+
+  @Get('/:id')
+  getById(@Param('id') movieId: number): Movie {
+    return this.moviesService.getOne(movieId);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: number): boolean {
+    return this.moviesService.deleteOne(movieId);
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: number, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
+  }
+}
