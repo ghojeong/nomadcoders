3c13f1eb074935508ab223b6ea348b87f05f7109
Pyro(JeongWan Gho) <rhwjddhks@gmail.com>
2025-03-12 18:19:39 +0900

    test: movies.service 삭제 테스트

--- hi-nest/src/movies/movies.service.spec.ts
+++ hi-nest/src/movies/movies.service.spec.ts
@@ -47,4 +47,42 @@ describe('MoviesService', () => {
       }
     });
   });
+
+  describe('deleteOne', () => {
+    it('deletes a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+
+      const beforeDelete = service.getAll().length;
+      service.deleteOne(1);
+      const afterDelete = service.getAll().length;
+
+      expect(afterDelete).toBeLessThan(beforeDelete);
+    });
+
+    it('should return a 404', () => {
+      try {
+        service.deleteOne(1);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual(`Movie with ID ${1} not found.`);
+      }
+    });
+  });
+
+  describe('create', () => {
+    it('should create a movie', () => {
+      const befeoreCreate = service.getAll().length;
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const afterCreate = service.getAll().length;
+      expect(afterCreate).toBeGreaterThan(befeoreCreate);
+    });
+  });
 });
