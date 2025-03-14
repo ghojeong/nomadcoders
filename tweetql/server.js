0946f614298cce401ebf7f733830408f6796d004
Pyro(JeongWan Gho) <rhwjddhks@gmail.com>
2025-03-14 14:51:28 +0900

    feat: graphql 의 mutation 연습

--- tweetql/server.js
+++ tweetql/server.js
@@ -1,16 +1,92 @@
 import { ApolloServer, gql } from "apollo-server";
 
+let tweets = [
+  {
+    id: "1",
+    text: "first one",
+  },
+  {
+    id: "2",
+    text: "second one",
+  },
+];
+
+let users = [
+  {
+    id: "1",
+    firstName: "one",
+    lastName: "first",
+  },
+  {
+    id: "2",
+    firstName: "two",
+    lastName: "second",
+  },
+];
+
 const typeDefs = gql`
   type Query {
-    text: String
-    hello: String
+    ping: String!
+    allUsers: [User!]!
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
+  }
+  type Mutation {
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
   }
   type Tweet {
-    text: String
+    id: ID!
+    text: String!
+    author: User
+  }
+  type User {
+    id: ID!
+    firstName: String!
+    lastName: String!
+    fullName: String!
   }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    ping() {
+      return "pong";
+    },
+    allTweets() {
+      return tweets;
+    },
+    allUsers() {
+      return users;
+    },
+    tweet(_, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
+    },
+  },
+  Mutation: {
+    postTweet(_, { text, userId }) {
+      const newTweet = {
+        id: tweets.length + 1,
+        text,
+      };
+      tweets.push(newTweet);
+      return newTweet;
+    },
+    deleteTweet(_, { id }) {
+      const tweet = tweets.find((tweet) => tweet.id === id);
+      if (!tweet) return false;
+      tweets = tweets.filter((tweet) => tweet.id !== id);
+      return true;
+    },
+  },
+  User: {
+    fullName({ firstName, lastName }) {
+      return `${firstName} ${lastName}`;
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
