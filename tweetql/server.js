1e439bed7d12793ec9eb41b021849d8ae0441722
Pyro(JeongWan Gho) <rhwjddhks@gmail.com>
2025-03-14 13:38:35 +0900

    init graphql project

---
+++ tweetql/server.js
@@ -0,0 +1,17 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    text: String
+    hello: String
+  }
+  type Tweet {
+    text: String
+  }
+`;
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(({ url }) => {
+  console.log(`Running on ${url}`);
+});
